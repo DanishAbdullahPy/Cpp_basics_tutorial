b11363daae4a8df5b2e4b7e2c1c8486c22f7b8de
Danish Abdullah <danishabdullah276@gmail.com>
2023-09-26 00:57:19 +0530

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -6,6 +6,8 @@ string name  = "DAYAM";
 cout<<"The name is " <<name<<endl;
 cout<<"The length of the name is "<<name.length()<<endl;
 cout<<"The name is "<<name.substr(1,115)<<endl;
-cout<<"The name is "<<name.substr(2,4)<<endl;
+//cout<<"The name is "<<name.substr(2,4)<<endl;
+  cout << "The name is " << name.substr(1, 4) << endl;
+
 
 }
